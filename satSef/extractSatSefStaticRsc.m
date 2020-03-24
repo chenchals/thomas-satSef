dae4330b784434afbe9f8c384298c9a9763ddde3
Chenchal Subraveti <chenchals@gmail.com>
2020-03-24 11:20:38 -0500

    Create static 150 ms rsc extract from all area-pairs

--- satSef/extractSatSefStaticRsc.m
+++ satSef/extractSatSefStaticRsc.m
@@ -65,24 +65,82 @@ corrDatFields = {
     'alignedTimeWin'     
     'trialNosByCondition'
     'xBaselineMeanStd' 
-    'yBaselineMeanStd' 
+    'yBaselineMeanStd'
+    'rho_pval_win_150ms' 
     'critRho10'          
     'critRho05'          
-    'critRho01'                  
-    'rho_pval_win_50ms'     
-    'rho_pval_static_50ms'    
-    'rho_pval_static_Z_baseline_50ms'  
-    'rho_pval_static_Z_trial_50ms' 
-    'rho_pval_win_150ms'     
+    'critRho01' 
     'rho_pval_static_150ms'    
-    'rho_pval_static_Z_baseline_150ms'  
-    'rho_pval_static_Z_trial_150ms' 
-    'rho_pval_win_200ms'     
-    'rho_pval_static_200ms'    
-    'rho_pval_static_Z_baseline_200ms'  
-    'rho_pval_static_Z_trial_200ms' 
-    'xWaves'
-    'yWaves'
-    'xWaveWidths'
-    'yWaveWidths'
+    'critRho10_Z_baseline'
+    'critRho05_Z_baseline'
+    'critRho01_Z_baseline'
+    'rho_pval_static_Z_baseline_150ms'
     };
+t = regexp(corrDatFields,'rho_pval_static_(\d*)ms$','tokens');
+staticWinSizes = sort(cellfun(@(x) str2double(x{1}),t(~cellfun(@isempty,t))));
+spkCorrStatic = struct();
+nThreads = 0;
+p = gcp('nocreate');
+if numel(p) == 1
+    nThreads = p.NumWorkers;
+end
+tic
+for d = 1:numel(corrMatDirs)
+    areaPair = areaPairs{d};
+    fprintf('Doing pairs for %s...',areaPair);
+    areaPairField = strrep(areaPair,'-','_');
+    srcFiles = dir([corrMatDirs{d},'/spkCorr_PAIR_*.mat']);
+    srcFiles = strcat({srcFiles.folder}','/',{srcFiles.name}');
+    nPairs = numel(srcFiles);
+    outPairs = struct();
+    parfor (p = 1:nPairs, nThreads)
+    %for p = 1:nPairs
+        pDat = table();
+        srcFile = srcFiles{p};
+        pDatStruct = load(srcFile);
+        [~,fn,ext] = fileparts(srcFile);
+        srcFile = [fn ext]; 
+        nRows = size(pDatStruct.spkCorr,1);
+        temp = [repmat(pDatStruct.cellPairInfo,nRows,1) pDatStruct.spkCorr];
+        tempFns = temp.Properties.VariableNames';
+        pDat.srcFile = repmat({srcFile},nRows,1);
+        pDat.pairAreas = repmat({areaPair},nRows,1);
+        pDat.nTrials = cellfun(@(x) numel(x), temp.trialNosByCondition);
+        % temp.trialNosByCondition = [];
+        fieldIdx = cell2mat(cellfun(@(x) find(strcmp(tempFns,x)),corrDatFields,'UniformOutput',false));
+        pDat = [pDat,temp(:,corrDatFields)];
+        pDat.XY_Dist = cell2mat(pDat.XY_Dist);
+        % split rho, pval
+        for sW = 1:numel(staticWinSizes)
+            staticWin = staticWinSizes(sW);
+            swSuffix = num2str(staticWin,'_%dms');
+            % Use rho/pval from raw counts            
+            swRhoPval = pDat.(['rho_pval_static' swSuffix]);
+            [rho,pval,sig05,sig01] = cellfun(@(x) deal(x(1),x(2),x(2)<=0.05,x(2)<=0.01),swRhoPval);            
+            [pDat.(['rhoRaw' swSuffix]),...
+            pDat.(['pvalRaw' swSuffix]),...
+            pDat.(['signifRaw_05' swSuffix]),...
+            pDat.(['signifRaw_01' swSuffix])] = deal(rho,pval,sig05,sig01);
+            % Use rhp/pval from Z-scored (with mean and std from) Baseline period
+            swRhoPval = pDat.(['rho_pval_static_Z_baseline' swSuffix]);
+            [rho,pval,sig05,sig01] = cellfun(@(x) deal(x(1),x(2),x(2)<=0.05,x(2)<=0.01),swRhoPval);            
+            [pDat.(['rhoZBaseline' swSuffix]),...
+            pDat.(['pvalZBaseline' swSuffix]),...
+            pDat.(['signifZBaseline_05' swSuffix]),...
+            pDat.(['signifZBaseline_01' swSuffix])] = deal(rho,pval,sig05,sig01);
+        end
+                
+        outPairs(p).pDat = pDat;
+    end
+    
+    spkCorrStatic.(areaPairField) = vertcat(outPairs.pDat); 
+    fprintf('Done %.3f sec.\n',toc)
+end
+size(spkCorrStatic);
+[d,~,~]=fileparts(oFilename);
+if ~exist(d,'dir')
+    mkdir(d);
+end
+save(oFilename,'-v7.3','-struct','spkCorrStatic');
+toc
+
