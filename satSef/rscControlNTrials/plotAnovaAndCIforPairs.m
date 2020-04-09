a5224c99a5f17e5c245b6c12fba27a5874e12b79
Chenchal Subraveti <chenchals@gmail.com>
2020-04-08 21:19:37 -0500

    Updated plots and estimated Rsc,

--- satSef/rscControlNTrials/plotAnovaAndCIforPairs.m
+++ satSef/rscControlNTrials/plotAnovaAndCIforPairs.m
@@ -24,26 +24,15 @@ rscData.absRhoEst80 = abs(spkCorr.rhoEstRaw_nTrials_80);
 %% Get statistics for the specific rho values
 groupCols = {'condition','satCondition','outcome'};
 rhocols = {'absRho','absRhoEst40','absRhoEst80'};
-rscDataStats = grpstats(rscData(:,[groupCols rhocols]),groupCols,{'mean','sem'});
-rscDataStats = sortrows(rscDataStats,{'outcome','satCondition'});
-rscDataStats.Properties.RowNames = {};
+
+rscSatConditionStats = grpstats(rscData(:,['satCondition', rhocols]),'satCondition',{'mean','std','sem'});
+rscSatConditionStats.Properties.RowNames = {};
+
+rscOutcomesStats = grpstats(rscData(:,[groupCols rhocols]),groupCols,{'mean','std','sem'});
+rscOutcomesStats = sortrows(rscOutcomesStats,{'outcome','satCondition'});
+rscOutcomesStats.Properties.RowNames = {};
 % display 3 groups of 2 bars each
 % data = [1 2;3 4;5 6]
-%rsc groups
-% rscDataStats =
-% 
-%   6×10 table
-% 
-%           condition          satCondition       outcome       GroupCount    mean_absRho    sem_absRho    mean_absRhoEst40    sem_absRhoEst40    mean_absRhoEst80    sem_absRhoEst80
-%     _____________________    ____________    _____________    __________    ___________    __________    ________________    _______________    ________________    _______________
-% 
-%     'AccurateCorrect'         'Accurate'     'Correct'           258         0.082981      0.0042224         0.081999           0.0042545           0.082866           0.0042311   
-%     'FastCorrect'             'Fast'         'Correct'           258         0.082178      0.0046837         0.080563           0.0046447           0.081294           0.0047255   
-%     'AccurateErrorChoice'     'Accurate'     'ErrorChoice'       258           0.1174      0.0054808          0.11567           0.0054272            0.11709           0.0054492   
-%     'FastErrorChoice'         'Fast'         'ErrorChoice'       258          0.12106      0.0068834          0.12054           0.0069491            0.12085           0.0068788   
-%     'AccurateErrorTiming'     'Accurate'     'ErrorTiming'       258          0.10216      0.0054097          0.10115           0.0053992            0.10155           0.0054007   
-%     'FastErrorTiming'         'Fast'         'ErrorTiming'       258           0.1621      0.0082477          0.15845           0.0081643            0.15909           0.0081793   
-
 % quick:
 accClr = [1 0.2 0.2];
 fasClr = [0.2 1.0 0.2];
@@ -52,10 +41,10 @@ grpColors = {accClr;fasClr};
 
 outcomes = {'Correct','ErrorChoice','ErrorTiming'}';
 sat = {'Accurate','Fast'};
-idx = ismember(rscDataStats.satCondition,'Accurate');
-accuTbl = rscDataStats(idx,{'outcome','mean_absRho','sem_absRho'});
-idx = ismember(rscDataStats.satCondition,'Fast');
-fastTbl = rscDataStats(idx,{'outcome','mean_absRho','sem_absRho'});
+idx = ismember(rscOutcomesStats.satCondition,'Accurate');
+accuTbl = rscOutcomesStats(idx,{'outcome','mean_absRho','sem_absRho'});
+idx = ismember(rscOutcomesStats.satCondition,'Fast');
+fastTbl = rscOutcomesStats(idx,{'outcome','mean_absRho','sem_absRho'});
 figure
 [barCentersTbl, errBarHandles] = plotGroupedBarsWithErrs(accuTbl.outcome,...
     [accuTbl.mean_absRho fastTbl.mean_absRho],...
@@ -67,52 +56,94 @@ figure
 barCenter = barCentersTbl.ErrorTiming(1);
 idx = ismember(rscData.condition,'AccurateErrorTiming');
 ci = getCi(abs(rscData.rhoEst40(idx)));
-overplotBox(barCenter,ci,'k');
-ptile = prctile(abs(rscData.rhoEst40(idx)),[10 90]);
-%overplotBox(barCenter,ptile,'k');
+overplotBox(barCenter,ci,'k','-');
+%ptile = prctile(abs(rscData.rhoEst40(idx)),[10 90]);
+%overplotBox(barCenter,ptile,'k','-');
 
 % Fast_Error_Choice ci/percentile 10/90 for 80 subsamples
 idx = ismember(rscData.condition,'FastErrorChoice');
 barCenter = barCentersTbl.ErrorChoice(2);
 ci = getCi(abs(rscData.rhoEst80(idx)));
-overplotBox(barCenter,ci,'k');
-ptile = prctile(abs(rscData.rhoEst80(idx)),[10 90]);
-%overplotBox(barCenter,ptile,'k');
+%overplotBox(barCenter,ci,'k',':');
+%ptile = prctile(abs(rscData.rhoEst80(idx)),[10 90]);
+overplotBox(barCenter,ptile,'k',':');
 
 % Accurate_Correct percentile 10/90 for 80 subsamples
 idx = ismember(rscData.condition,'AccurateCorrect');
 barCenter = barCentersTbl.Correct(1);
 ci40 = getCi(abs(rscData.rhoEst40(idx)));
-overplotBox(barCenter,ci40,'k');
+overplotBox(barCenter,ci40,'k','-');
 ci80 = getCi(abs(rscData.rhoEst80(idx)));
-%overplotBox(barCenter,ci80,'k');
+overplotBox(barCenter,ci80,'k',':');
 
 % Fast_Correct percentile 10/90 for 80 subsamples
 idx = ismember(rscData.condition,'FastCorrect');
 barCenter = barCentersTbl.Correct(2);
 ci40 = getCi(abs(rscData.rhoEst40(idx)));
-overplotBox(barCenter,ci40,'k');
+overplotBox(barCenter,ci40,'k','-');
 ci80 = getCi(abs(rscData.rhoEst80(idx)));
-overplotBox(barCenter,ci80,'k');
+overplotBox(barCenter,ci80,'k',':');
+ppretty([3.5,3])
+%% DO 2-Factor ANOVA satCondition by outcome
+temp = rscData;
+temp.epoch = repmat({'PostSaccade'},size(rscData,1),1);
+[anovRes] = doAnovaConditionByOutcome(temp,'absRho');
+
+anovaTbl = anovRes.PostSaccade.anovaTbl
+%satConditionTbl = anovRes.PostSaccade.satCondition(:,[1:6,9])
+%outcomeTbl = anovRes.PostSaccade.outcome(:,[1:6,9])
+satConditionByOutcome = sortrows(anovRes.PostSaccade.satCondition_outcome(:,[1:6 9]),{'levelName1','levelName2'},{'ascend','descend'})
+
+outcomeStats = rscOutcomesStats(:,[1,5,6,8,9,11,12])
+satStats = rscSatConditionStats(:,[1,3,4,6,7,9,10])
+satStats.Properties.VariableNames = outcomeStats.Properties.VariableNames;
+allStats = [satStats;outcomeStats]
 
 
 
+%% Do 2-way anova
+function [anovaConditionByOutcome] = doAnovaConditionByOutcome(spkCorr,useRho)
+% Do 2-Factor anova for the following -
+%    Factor1: condition
+%    Factor 2: outcome
+% The data colums for input anovaTbl from spkCorr data above:
+%   yVals     : rho or rhoZ
+%   condition : Factor-1 for 2-Factor anova values
+%                 Fast|Accurate
+%   outcome   : Factor-2 for 2-Factor anova values:
+%                 Correct|ErrorChoice|ErrorTiming
 
-% xBeginEnd = barCentersTbl.ErrorTiming(1) + [-0.125 0.125];
-% yBeginEnd = ci;
-% xVec = [xBeginEnd fliplr(xBeginEnd)];
-% yVec = [yBeginEnd;yBeginEnd];
-% yVec = yVec(:)';
-% pH = patch(xVec,yVec,[0.9 0.9 0.9],'FaceAlpha',0.1)
+epochs = {'PostSaccade'};
+anovaConditionByOutcome = struct();
+    for ep = 1:numel(epochs)
+        epoch = epochs{ep};
+        idx = ismember(spkCorr.epoch,epoch);
+        anovaTbl = table();
+        anovaTbl.rho = spkCorr{idx,useRho};
+        % 2-way anova satcondition by outcome
+        % Factor-1
+        anovaTbl.satCondition = spkCorr{idx,'satCondition'};
+        % Factor-2
+        anovaTbl.outcome = spkCorr{idx,'outcome'};
+        % do anova
+        anovaModel = 'interaction'; %  [linear|interaction]
+        multiCompareFlag = 1; % [0|1]
+        alpha = 0.05;
+        fn = epoch;
+        anovaConditionByOutcome.(fn) = satAnova(anovaTbl,anovaModel,multiCompareFlag,alpha);
+    end
+end
+
 
 %%
-function [pH] = overplotBox(barCenter,yBeginEnd,edgeColor)
+function [pH] = overplotBox(barCenter,yBeginEnd,edgeColor,lineStyle)
 xBeginEnd = barCenter + [-0.125 0.125];
 xVec = [xBeginEnd fliplr(xBeginEnd)];
 yVec = [yBeginEnd;yBeginEnd];
 yVec = yVec(:)';
 pH = patch(xVec,yVec,[1 1 1],'FaceAlpha',0.0);
 set(pH,'EdgeColor',edgeColor)
+set(pH,'LineStyle',lineStyle)
 set(pH,'LineWidth',0.5)
 
 end
